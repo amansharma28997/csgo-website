c36104f8f5ba34781bd6653037c534da69306efe
blz97light <amansharma.28997@gmail.com>
2022-03-03 09:56:03 +0530

    adding content to ranking page

---
+++ src/components/ranking.js
@@ -0,0 +1,67 @@
+export default function Ranking() {
+  return (
+    <>
+      data
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+      <br />
+      line2
+      <br />
+      line3
+    </>
+  );
+}
