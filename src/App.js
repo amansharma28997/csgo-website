43b82c11a3e0e4f82f4d4103400beeb4daf57194
blz97light <amansharma.28997@gmail.com>
2022-07-26 23:14:35 +0530

    adding last update date

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Ranking from "./components/ranking";
+import LastUpdateDate from "./components/lastupdate";
+
 function App() {
   return (
     <>
       <Header />
       <Ranking />
+      <LastUpdateDate />
       <Footer />
     </>
   );
---
+++ src/components/lastupdate.js
@@ -0,0 +1,17 @@
+import Typography from "@mui/material/Typography";
+export default function LastUpdateDate() {
+  return (
+    <>
+      <br />
+      <Typography
+        variant="body1"
+        color="#f44336"
+        component="div"
+        align="right"
+        style={{ fontWeight: "400", marginLeft: "5vw", marginRight: "5vw" }}
+      >
+        *Last Rank update: 20 July 2022
+      </Typography>
+    </>
+  );
+}
